88d6dfa8a41ee6ee492347b2b0d82462d47954b3
AzizullahCodes <azizullahaziz7777@gmail.com>
2025-04-10 21:55:26 +0500

    y

---
+++ app.js
@@ -0,0 +1,15 @@
+// // Select all buttons with the class 'common-button'
+// const buttons = document.querySelectorAll('.btn');
+
+// // Function to handle the click event
+// function handleClick(event) {
+//   // 'event.target' refers to the clicked button
+//   console.log(`Button clicked: ${event.target.textContent}`);
+//   // Add your desired functionality here
+// }
+
+// // Loop through each button and attach the click event listener
+// buttons.forEach(button => {
+//   button.addEventListener('click', handleClick);
+// });
+ 
